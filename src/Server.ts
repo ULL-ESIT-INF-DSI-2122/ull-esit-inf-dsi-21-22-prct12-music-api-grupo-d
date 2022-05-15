a3862324295c7d023a31abab7fcc039ce1f2e08d
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2022-05-15 13:43:49 +0100

    Añadiendo endpoint para el reinicio de la base de datos en modo test

--- src/Server.ts
+++ src/Server.ts
@@ -6,6 +6,9 @@ import { Request, Response } from "express";
 import { join } from "path";
 
 import routes from "./routes/index";
+import { Song } from "./models/song";
+import { Artist } from "./models/artist";
+import { Playlist } from "./models/playlist";
 
 export default class Server {
   private app: express.Application;
@@ -21,17 +24,30 @@ export default class Server {
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use(express.static(join(__dirname, "../public")));
 
+    if (process.env.NODE_ENV === "test") {
+      this.app.get("/reset", async (_: Request, res: Response) => {
+        await Song.deleteMany({})
+        await Artist.deleteMany({})
+        await Playlist.deleteMany({})
+
+        res.status(200).json({message: "Reset OK"})
+      })
+    }
+
     this.app.use(routes);
   };
 
   connectMongoDB = () => {
+    let MONGODB_URI: string = `mongodb+srv://admin:${process.env.PWDMONGO}@music-dsi.oqoxo.mongodb.net/Music-API?retryWrites=true&w=majority`;
+    
+    if (process.env.NODE_ENV === "test") {
+      MONGODB_URI = `mongodb+srv://admin:${process.env.PWDMONGO}@music-dsi.oqoxo.mongodb.net/Music-API-Test?retryWrites=true&w=majority`;
+    }    
+    
     const options = {
       autoIndex: true,
       useNewUrlParser: true,
     };
-
-    const MONGODB_URI: string = `mongodb+srv://admin:${process.env.PWDMONGO}@music-dsi.oqoxo.mongodb.net/Music-API?retryWrites=true&w=majority`;
-
     mongoose
       .connect(MONGODB_URI, options)
       .then((_) => console.log("Database connected!"))
